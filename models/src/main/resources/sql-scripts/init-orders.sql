280c9391c48523fc7fb904e9f264ce8b5463662b
Nemanja <soldo.nemanja@gmail.com>
2019-01-06 17:01:33 +0100

    1

---
+++ models/src/main/resources/sql-scripts/init-orders.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (car_id, customer_id,time_from,time_to, pickup_location,drop_location) VALUES (1,2, TIMESTAMP '2017-03-01 11:23:38',TIMESTAMP '2017-03-03 18:23:38', 'Ljubljana', 'Kranj');
+INSERT INTO orders (car_id, customer_id,time_from,time_to, pickup_location,drop_location) VALUES (3,1, TIMESTAMP '2017-06-01 11:55:00',TIMESTAMP '2017-06-12 21:20:00', 'Ljubljana', 'Portoroz');
+
