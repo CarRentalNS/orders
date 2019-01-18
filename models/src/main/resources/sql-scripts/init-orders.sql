b74c702ece4579782c0c1e201f29c87495275b83
Nemanja <soldo.nemanja@gmail.com>
2019-01-18 12:08:39 +0100

    connected with feedback

--- models/src/main/resources/sql-scripts/init-orders.sql
+++ models/src/main/resources/sql-scripts/init-orders.sql
@@ -1,3 +1,3 @@
 INSERT INTO orders (car_id, customer_id,time_from,time_to, pickup_location,drop_location) VALUES (1,2, TIMESTAMP '2017-03-01 11:23:38',TIMESTAMP '2017-03-03 18:23:38', 'Ljubljana', 'Kranj');
 INSERT INTO orders (car_id, customer_id,time_from,time_to, pickup_location,drop_location) VALUES (3,1, TIMESTAMP '2017-06-01 11:55:00',TIMESTAMP '2017-06-12 21:20:00', 'Ljubljana', 'Portoroz');
-
+INSERT INTO orders (car_id, customer_id,time_from,time_to, pickup_location,drop_location) VALUES (3,3, TIMESTAMP '2018-06-01 11:55:00',TIMESTAMP '2018-06-12 21:20:00', 'Portoroz', 'Ljubljana');
